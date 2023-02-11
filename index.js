d11f3f69082c41d1e25ae604e5c024341863a5f6
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-12 00:13:52 +0100

    Setup App

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}); 
+
+app.get('*', (req, res) => {
+  res.send('Route not found');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on Port:${port}`);
+});
