f5071242432e09decfc327948c66b11cb088032d
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-12 13:20:07 +0100

    Setup Ratelimiter

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const { limiter } = require('./middleware');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(limiter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
